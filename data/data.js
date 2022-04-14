c0e17c9670a51e61771c95f85c7f8027cad0324d
vovoka <vovoka.path@gmail.com>
2022-04-14 15:34:13 +0300

    feat: add mylessons data

--- data/data.js
+++ data/data.js
@@ -1,11 +1,13 @@
 import presentsimple from "./present-simple.js";
 import presentperfect from "./present-perfect.js";
+import mylessons from "./mylessons.js";
 import expressingthoughts from "./expressing-thoughts.js";
 import introductoryexpressions from "./introductory-expressions.js";
 
 export const data = {
   presentsimple,
   presentperfect,
+  mylessons,
   expressingthoughts,
   introductoryexpressions,
 };
